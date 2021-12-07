6b7b35f0f4043966e51e579cfff17a3d35cf6f2f
RodrigoFortunato <rodrigofortunatocp2@gmail.com>
2021-12-07 18:41:06 -0300

    Add files of mappings

--- src/Thor.Data/Context/MyDbContext.cs
+++ src/Thor.Data/Context/MyDbContext.cs
@@ -1,8 +1,16 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Thor.Business.Models;
+
 namespace Thor.Data.Context
 {
-    public class MyDbContext
+    public class MyDbContext : DbContext
     {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
     }
 }
